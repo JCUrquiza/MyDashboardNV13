7bb4c439db0adce78aac1a3fa5412fe0682797b8
Juan <gurquiza8@gmail.com>
2024-02-11 14:42:56 -0600

    use client page on counterpage

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,30 @@
+'use client';
+
+import { useState } from 'react';
 
 export default function CounterPage() {
+
+    const [count, setCount] = useState(5);
+
     return (
-        <div>
-            <h1>CounterPage</h1>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <span>Productos en el carrito</span>
+            <span className="text-9xl">{ count }</span>
+
+            <div className="flex">
+                <button 
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2" 
+                    onClick={ () => setCount( count + 1 ) }
+                >
+                    + 1
+                </button>
+                <button 
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2" 
+                    onClick={ () => setCount( count - 1 ) }
+                >
+                    - 1
+                </button>
+            </div>
         </div>
     )
 }
