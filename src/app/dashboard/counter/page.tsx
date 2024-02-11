c4e82055bd6173bdaba6ab3dde00adb18cad2a2c
Juan <gurquiza8@gmail.com>
2024-02-11 15:05:59 -0600

    Creating a new file for correct use from serversiderendering

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,30 +1,18 @@
-'use client';
+import { CartCounter } from '@/app/shopping-cart';
 
-import { useState } from 'react';
+export const metadata = {
+    title: 'Counter Page',
+    description: 'Un simple contador'
+}
 
 export default function CounterPage() {
-
-    const [count, setCount] = useState(5);
 
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
             <span>Productos en el carrito</span>
-            <span className="text-9xl">{ count }</span>
+            
+            <CartCounter value={ 20 } />
 
-            <div className="flex">
-                <button 
-                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2" 
-                    onClick={ () => setCount( count + 1 ) }
-                >
-                    + 1
-                </button>
-                <button 
-                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2" 
-                    onClick={ () => setCount( count - 1 ) }
-                >
-                    - 1
-                </button>
-            </div>
         </div>
     )
 }
