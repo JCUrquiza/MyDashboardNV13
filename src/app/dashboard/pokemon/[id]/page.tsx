8fec735227468cfc3bf586d1775a90709199bcc7
Juan <gurquiza8@gmail.com>
2024-02-13 14:30:25 -0600

    Adding dinamic metadata

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import { Metadata } from 'next';
+import { Pokemon } from '@/pokemons';
 
 interface Props {
     params: { id: string };
 }
 
-export default function PokemonPage({ params }: Props) {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+
+    const { id, name } = await getPokemon(params.id);
+
+    return {
+        title: `#${id} - ${name}`,
+        description: `Página del pokemón: ${name}`
+    }
+
+}
+
+const getPokemon = async(id: string): Promise<Pokemon> => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,{
+        cache: 'force-cache'
+    }).then(res => res.json());
+
+    return pokemon;
+    
+}
+
+export default async function PokemonPage({ params }: Props) {
+
+    const pokemon = await getPokemon(params.id);
 
     return (
-        <div>Pokemon { params.id }</div>
+        <div>
+            <div>Pokemon { params.id }</div>
+            <div>
+                { pokemon.name }
+            </div>
+        </div>
     )
 }
