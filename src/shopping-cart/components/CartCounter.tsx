aafff4db9dd809e5e4d53179d24669209d0782f5
Juan <gurquiza8@gmail.com>
2024-02-17 15:00:59 -0600

    Exercise with count finished

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -8,14 +8,29 @@ interface Props {
     value?: number;
 }
 
+export interface CounterResponse {
+    count: number;
+}
+
+const getApiCounter = async() => {
+    const data = await fetch('/api/counter').then( res => res.json());
+    return data as CounterResponse;
+}
+
 export const CartCounter = ({ value = 10 }: Props) => {
 
     const count = useAppSelector( state => state.counter.count );
     const dispatch = useAppDispatch();
 
+    // useEffect(() => {
+    //     dispatch( initCounterState(value) );
+    // }, [dispatch, value]);
+
     useEffect(() => {
-        dispatch( initCounterState(value) );
-    }, [dispatch, value]);
+        getApiCounter()
+            .then( ({ count }) => dispatch( initCounterState(count) ) );
+    }, [dispatch]);
+    
     
     return (
         <>
