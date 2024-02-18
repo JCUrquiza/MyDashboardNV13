b6abf7bad5abe18c23de7d1daedb3d2c91177113
Juan <gurquiza8@gmail.com>
2024-02-17 19:07:44 -0600

    Showing list of favorite pokemons

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-import { PokemonGrid } from '@/pokemons';
+import { FavoritePokemons } from '@/pokemons';
 
 export const metadata: Metadata = {
     title: 'Favoritos',
@@ -13,7 +13,7 @@ export default async function PokemonsPage() {
 
             <span className='text-5xl my-2'>Pókemons Favoritos<small className='text-blue-500'>Global State</small></span>
 
-            <PokemonGrid pokemons={[]} />
+            <FavoritePokemons />
 
         </div>
     )
